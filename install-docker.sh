84646c68b058d81950feaf06cf2fc2fa038ffc01
Mahendhar Kanuganti <kanuganti98@gmail.com>
2024-06-23 13:47:47 +0530

    docker-expense

---
+++ install-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Check wether root user or not
+
+R="\e[31m"
+N="\e[0m"
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl status docker
+systemctl enable docker
+
+## add the ec2-user into docker group, to run the docker commands without root user.
+usermod -aG docker ec2-user
+#logout and login again into server to effect the changes
+echo -e "$R Logout and Login again $N"
